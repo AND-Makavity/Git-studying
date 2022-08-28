7230419fbf9b66acf6bcadcf965ccababe5cda6b
And <www-and@yandex.ru>
2022-08-28 07:47:54 +0300

    Min Max of Two Numbers

--- First lesson/Program.cs
+++ First lesson/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿int first = new Random().Next(1, 10);
+int second = new Random().Next(1, 10);
+int max = 0;
+int min = 0;
+
+if (first > second)
+{
+    max = first;
+    min = second;
+}
+else
+{
+    min = first;
+    max = second;
+}
+Console.WriteLine($"first = {first}");
+Console.WriteLine($"second = {second}");
+Console.WriteLine($"max = {max}");
+Console.WriteLine($"min = {min}");
