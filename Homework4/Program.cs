af3d84a2446521384990ea9f19d89151f7fdd569
And <www-and@yandex.ru>
2022-08-28 08:29:37 +0300

    Even Numbers up to N

---
+++ Homework4/Program.cs
@@ -0,0 +1,8 @@
+﻿int N = new Random().Next(1, 20);
+Console.WriteLine($"Программа показывает все чётные числа от 1 до {N}");
+int index;
+
+for (index = 1; index <= N; index++)
+{
+    if (index % 2 == 0) Console.Write($"{index} ");
+}
