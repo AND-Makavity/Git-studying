fb9636e4336b009b3d6426c55c77f236a20c5df0
And <www-and@yandex.ru>
2022-08-28 08:03:47 +0300

    Max of Three

---
+++ Homework2/Program.cs
@@ -0,0 +1,10 @@
+﻿int a1 = new Random().Next(1, 10);
+int a2 = new Random().Next(1, 10);
+int a3 = new Random().Next(1, 10);
+int max = a1;
+
+if (a2 > max) max = a2;
+if (a3 > max) max = a3;
+
+Console.WriteLine($"{a1}, {a2}, {a3}");
+Console.WriteLine($"max = {max}");
